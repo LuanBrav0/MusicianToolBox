5d9d4f9dad4c9cbe28aa34c1dcf0df9f43658a35
LuanBrav0 <contatotropicaldesign@gmail.com>
2022-03-14 15:08:02 -0300

    now using pipenv

--- musiciantoolbox/musiciantoolbox/settings.py
+++ musiciantoolbox/musiciantoolbox/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '57)7l@-!fzqmwhj#(f331e!lv=405d1lrb7l-l@*xf+96d(r$='
+SECRET_KEY = 'jg2+n$r1)(ei62s!!qbin^)-wqae8&=5o*!ry&h4qb4mx&)mcw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
