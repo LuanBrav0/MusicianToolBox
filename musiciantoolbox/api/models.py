47bad65e3e32222d66f513a24900ebe594200214
LuanBrav0 <contatotropicaldesign@gmail.com>
2022-03-15 10:32:19 -0300

    commiting

--- musiciantoolbox/api/models.py
+++ musiciantoolbox/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Metronome(models.Model):
+    code = models.CharField(max_length=4, default="", unique=True)
+    host = models.CharField(max_length=25, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
